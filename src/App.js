964c2de3c7f897cbc85ba227c0f76beaa4f4ca0c
Ahmad haq <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-08-16 22:57:08 -0700

    app done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
+
+
+
+function App() {
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route />
+
+
+
+    
+  </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/screens/home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import 'App.scss'
+
+export default function home() {
+  return (
+    <div>home</div>
+  )
+}
